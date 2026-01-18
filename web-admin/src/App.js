ce96bb9f8e165965b6bd26599db4c65ef3505341
asepsaefuddin <githubasep@gmail.com>
2026-01-18 10:20:34 +0700

    menghapus yang tidak perlu

--- web-admin/src/App.js
+++ web-admin/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
 import Items from './pages/Items';
